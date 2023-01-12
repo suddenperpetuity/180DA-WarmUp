f620dc88f660e06c89aad4ae53a9e431225ad984
suddenperpetuity <brsutton124@gmail.com>
2023-01-12 15:49:10 -0800

    first python script

---
+++ test.py
@@ -0,0 +1,10 @@
+# test script
+
+if __name__ = '__main__':
+    x = 'ECE_180_DA_DB'
+    if x == 'EE_180_DA_DB':
+        print('You are living in 2017')
+    else:
+        # this is a comment
+        x += ' - best class ever'
+        print(x)
